4c0347898729e536f01f43058b8922d488661748
AnnalizaTayo <annaliza.arboleda@yahoo.com>
2023-06-16 17:42:08 +0800

    fix error

--- src/components/404.js
+++ src/components/404.js
@@ -1,12 +1,12 @@
 // NotFound.js
 
 import React from 'react';
-import NotFound from '../imgs/404.png';
+import NotData from '../imgs/404.png';
 
 const NotFound = () => {
   return (
     <div className='NotFound'>
-      <img src={NotFound} alt="Hooman, I can't find what you're looking for." />
+      <img src={NotData} alt="Hooman, I can't find what you're looking for." />
     </div>
   );
 };
